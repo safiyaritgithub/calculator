421457cbec6b04dab6b7bbfa1cf02e6ceec3148c
safiyaritgithub <satyasafi79812@gmail.com>
2023-08-03 21:10:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Keypad from "./keypad";
+
+
+
 
 function App() {
+
+let [input,setInput] = useState("")
+function handleClick(value){
+
+  setInput(input+value)
+
+}
+
+function calculate(value){
+  let outptVal = eval(input)
+  setInput(outptVal)
+} 
+
+function handleClear(){
+  setInput("")
+}
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container flex justify-center items-center h-screen flex-col">
+      <h1 className="text-2xl mb-3 font-medium">Calculator app using react</h1>
+      <div className="calculator">
+        <input type="text" value={input} className="output rounded-tl-md rounded-tr-md  outline-none w-80  h-14 text-2xl py-2 px-6 bg-[#333333] text-white text-right"/>
+        <Keypad handleClick={handleClick} calculate={calculate} handleClear={handleClear} ></Keypad>
+      </div>
+
     </div>
+    
   );
 }
 
---
+++ src/keypad.js
@@ -0,0 +1,89 @@
+function Keypad({handleClick,calculate,handleClear}){
+return(
+  <div className="keypad">
+ <div>
+<button onClick={
+  ()=>{
+    handleClick("7")
+  }}
+ className="w-[80px] h-20 bg-gray-100 hover:bg-[#d9d9d9] text-lg cursor-pointer border ">7</button>
+<button onClick={
+  ()=>{
+    handleClick("8")
+  }} className="w-[80px] h-20 bg-gray-100 hover:bg-[#d9d9d9] text-lg cursor-pointer  border ">8</button>
+<button onClick={
+  ()=>{
+    handleClick("9")
+  }} className="w-[80px] h-20 bg-gray-100 hover:bg-[#d9d9d9] text-lg cursor-pointer border  ">9</button>
+<button onClick={
+  ()=>{
+    handleClick("/")
+  }} className=" operator w-[80px] h-20 bg-gray-100 hover:bg-[#c7790f] text-2xl cursor-pointer border"style={{ background: 'orange',color:'#ccc' }}>/</button>
+ </div>
+
+ <div>
+<button onClick={
+  ()=>{
+    handleClick("4")
+  }} className="w-[80px] h-20 bg-gray-100 hover:bg-[#d9d9d9] text-lg cursor-pointer border">4</button>
+<button onClick={
+  ()=>{
+    handleClick("5")
+  }} className="w-[80px] h-20 bg-gray-100 hover:bg-[#d9d9d9] text-lg cursor-pointer  border ">5</button>
+<button onClick={
+  ()=>{
+    handleClick("6")
+  }} className="w-[80px] h-20 bg-gray-100 hover:bg-[#d9d9d9] text-lg cursor-pointer border  ">6</button>
+<button onClick={
+  ()=>{
+    handleClick("*")
+  }} className=" operator w-[80px] h-20 bg-gray-100 hover:bg-[#c7790f] text-2xl cursor-pointer border" style={{ background: 'orange',color:'#ccc' }}>*</button>
+ </div>
+
+ <div>
+<button onClick={
+  ()=>{
+    handleClick("1")
+  }} className="w-[80px] h-20 bg-gray-100 hover:bg-[#d9d9d9] text-lg cursor-pointer border">1</button>
+<button onClick={
+  ()=>{
+    handleClick("2")
+  }} className="w-[80px] h-20 bg-gray-100 hover:bg-[#d9d9d9] text-lg cursor-pointer  border ">2</button>
+<button onClick={
+  ()=>{
+    handleClick("3")
+  }} className="w-[80px] h-20 bg-gray-100 hover:bg-[#d9d9d9] text-lg cursor-pointer border  ">3</button>
+<button onClick={
+  ()=>{
+    handleClick("-")
+  }} className=" operator w-[80px] h-20 bg-gray-100 hover:bg-[#c7790f] text-2xl cursor-pointer border" style={{ background: 'orange',color:'#ccc' }}>-</button>
+ </div>
+
+ <div>
+<button onClick={
+  ()=>{
+    handleClick("0")
+  }} className="w-[80px] h-20 bg-gray-100 hover:bg-[#d9d9d9] text-lg cursor-pointer border">0</button>
+<button  className=" fun-key w-[80px] h-20 bg-gray-100 hover:bg-[#aba9a9] text-lg cursor-pointer border" style={{ background: '#c9c8cc' }} onClick={
+  ()=>{
+    calculate()
+  }}>=</button>
+<button  className="fun-key  w-[80px] h-20 bg-gray-100 hover:bg-[#aba9a9] text-lg cursor-pointer border" style={{ background: '#c9c8cc' }} onClick={
+  ()=>{
+    handleClear()
+  }}
+>C</button>
+<button onClick={
+  ()=>{
+    handleClick("+")
+  }}
+   className=" operator w-[80px] h-20 bg-gray-100 hover:bg-[#c7790f] text-2xl cursor-pointer border" style={{ background: 'orange',color:'#ccc' }}>+</button>
+ </div>
+
+
+</div>
+)
+
+
+}
+export default Keypad
